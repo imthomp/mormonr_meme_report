0296c2cdf9c88fc244c911c3bf981796673abee5
Isaac <isaac@bhroberts.org>
2023-10-12 11:15:45 -0600

    closer to format Josh wants

--- generate_meme_report.py
+++ generate_meme_report.py
@@ -1,6 +1,7 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.lib import colors
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Image, Spacer, PageBreak
+from reportlab.platypus import SimpleDocTemplate, Image, Spacer
+from reportlab.platypus import Table, TableStyle, PageBreak
 from reportlab.lib.units import inch
 import sqlite3
 import os
@@ -9,8 +10,9 @@ import os
 pdf_filename = 'meme_report.pdf'
 page_size = letter
 
-# Create a PDF document
-doc = SimpleDocTemplate(pdf_filename, pagesize=page_size)
+# Create a PDF document with adjusted margins
+# You can adjust the margins as needed
+doc = SimpleDocTemplate(pdf_filename, pagesize=page_size, leftMargin=0.5*inch, rightMargin=0.5*inch, topMargin=0.75*inch, bottomMargin=0.75*inch)
 
 # Define data source (SQLite database)
 db_filename = 'memes.db'
@@ -22,7 +24,7 @@ meme_data = []
 conn = sqlite3.connect(db_filename)
 cursor = conn.cursor()
 
-# Retrieve the top 300 memes from the database
+# Retrieve the top 300 memes from the database, sorted by like count
 cursor.execute('SELECT date, local_file, likes_count FROM memes ORDER BY likes_count DESC LIMIT 300')
 
 # Fetch meme data
@@ -36,41 +38,42 @@ conn.close()
 # Initialize a list to store PDF content
 pdf_content = []
 
-# Define table headers
-table_headers = ['Date', 'Image', 'Likes']
-
-# Create a table with meme data
-table_data = [table_headers]  # Initialize table data
+# Split meme data into groups of 6 entries per page
+entries_per_page = 6
+pages = [meme_data[i:i + entries_per_page] for i in range(0, len(meme_data), entries_per_page)]
 
 # Define table style
 table_style = TableStyle([
-    ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
     ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-    ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-    ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-    ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-    ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+    ('BOTTOMPADDING', (0, 0), (-1, -1), 12),
 ])
 
-# Add meme data to the table
-image_path = os.path.join('twitter_data', 'memes')
-
-for meme in meme_data:
-    date, local_file, likes_count = meme
-    image_file = os.path.join(image_path, local_file)
-    if os.path.exists(image_file):
-        img = Image(image_file, width=1.25*inch, height=1.25*inch)  # Adjust image size here
-        table_data.append([date, img, f'Likes: {likes_count}'])
-
-# Create the table
-table = Table(table_data, colWidths=[2.5*inch, 3*inch, 1.0*inch])  # Adjust column width here
-
-# Apply the table style
-table.setStyle(table_style)
-
-# Add the table to the PDF content
-pdf_content.append(table)
+# Create a PDF page for each group of 6 entries
+for page_data in pages:
+    # Create a table for the page
+    table_data = []
+    
+    for meme in page_data:
+        date, local_file, likes_count = meme
+        image_file = os.path.join('twitter_data', 'memes', local_file)
+        if os.path.exists(image_file):
+            img = Image(image_file, width=2*inch, height=2*inch)  # Adjust image size here
+            img.hAlign = 'CENTER'
+            table_data.append([img])
+            table_data.append([date])
+            table_data.append([f'Likes: {likes_count}'])
+            table_data.append([Spacer(1, 0.2*inch)])  # Spacer to add some vertical space
+    
+    # Create the table for the page
+    table = Table(table_data)
+    table.setStyle(table_style)
+    
+    # Add the table to the PDF content
+    pdf_content.append(table)
+    
+    # Add a page break between groups of 6 entries
+    pdf_content.append(PageBreak())
 
 # Build the PDF document
 doc.build(pdf_content)
